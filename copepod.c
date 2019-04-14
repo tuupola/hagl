00d97daec749342bbef9a74238b2986ee0a570d2
Mika Tuupola <tuupola@appelsiini.net>
2019-04-14 13:24:26 +0300

    Rename pod_triangle() to pod_draw_triangle()

--- copepod.c
+++ copepod.c
@@ -467,7 +467,7 @@ void pod_fill_polygon(int16_t amount, int16_t *vertices, uint16_t color) {
     }
 }
 
-void pod_triangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color) {
+void pod_draw_triangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color) {
     int16_t vertices[6] = {x0, y0, x1, y1, x2, y2};
     pod_draw_polygon(3, vertices, color);
 };
