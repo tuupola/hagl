0170741697d051421b7e7d747453ae9fe7024e5a
Mika Tuupola <tuupola@appelsiini.net>
2020-04-25 22:53:03 +0700

    Convert tabs to spaces

--- include/tjpgd.h
+++ include/tjpgd.h
@@ -29,22 +29,22 @@ typedef long			int32_t;
 
 /* Error code */
 typedef enum {
-	JDR_OK = 0,	/* 0: Succeeded */
-	JDR_INTR,	/* 1: Interrupted by output function */
-	JDR_INP,	/* 2: Device error or wrong termination of input stream */
-	JDR_MEM1,	/* 3: Insufficient memory pool for the image */
-	JDR_MEM2,	/* 4: Insufficient stream input buffer */
-	JDR_PAR,	/* 5: Parameter error */
-	JDR_FMT1,	/* 6: Data format error (may be damaged data) */
-	JDR_FMT2,	/* 7: Right format but not supported */
-	JDR_FMT3	/* 8: Not supported JPEG standard */
+    JDR_OK = 0,	/* 0: Succeeded */
+    JDR_INTR,	/* 1: Interrupted by output function */
+    JDR_INP,	/* 2: Device error or wrong termination of input stream */
+    JDR_MEM1,	/* 3: Insufficient memory pool for the image */
+    JDR_MEM2,	/* 4: Insufficient stream input buffer */
+    JDR_PAR,	/* 5: Parameter error */
+    JDR_FMT1,	/* 6: Data format error (may be damaged data) */
+    JDR_FMT2,	/* 7: Right format but not supported */
+    JDR_FMT3	/* 8: Not supported JPEG standard */
 } JRESULT;
 
 
 
 /* Rectangular structure */
 typedef struct {
-	uint16_t left, right, top, bottom;
+    uint16_t left, right, top, bottom;
 } JRECT;
 
 
@@ -52,26 +52,26 @@ typedef struct {
 /* Decompressor object structure */
 typedef struct JDEC JDEC;
 struct JDEC {
-	uint16_t dctr;				/* Number of bytes available in the input buffer */
-	uint8_t* dptr;				/* Current data read ptr */
-	uint8_t* inbuf;				/* Bit stream input buffer */
-	uint8_t dmsk;				/* Current bit in the current read byte */
-	uint8_t scale;				/* Output scaling ratio */
-	uint8_t msx, msy;			/* MCU size in unit of block (width, height) */
-	uint8_t qtid[3];			/* Quantization table ID of each component */
-	int16_t dcv[3];				/* Previous DC element of each component */
-	uint16_t nrst;				/* Restart inverval */
-	uint16_t width, height;		/* Size of the input image (pixel) */
-	uint8_t* huffbits[2][2];	/* Huffman bit distribution tables [id][dcac] */
-	uint16_t* huffcode[2][2];	/* Huffman code word tables [id][dcac] */
-	uint8_t* huffdata[2][2];	/* Huffman decoded data tables [id][dcac] */
-	int32_t* qttbl[4];			/* Dequantizer tables [id] */
-	void* workbuf;				/* Working buffer for IDCT and RGB output */
-	uint8_t* mcubuf;			/* Working buffer for the MCU */
-	void* pool;					/* Pointer to available memory pool */
-	uint16_t sz_pool;			/* Size of momory pool (bytes available) */
-	uint16_t (*infunc)(JDEC*, uint8_t*, uint16_t);/* Pointer to jpeg stream input function */
-	void* device;				/* Pointer to I/O device identifiler for the session */
+    uint16_t dctr;				/* Number of bytes available in the input buffer */
+    uint8_t* dptr;				/* Current data read ptr */
+    uint8_t* inbuf;				/* Bit stream input buffer */
+    uint8_t dmsk;				/* Current bit in the current read byte */
+    uint8_t scale;				/* Output scaling ratio */
+    uint8_t msx, msy;			/* MCU size in unit of block (width, height) */
+    uint8_t qtid[3];			/* Quantization table ID of each component */
+    int16_t dcv[3];				/* Previous DC element of each component */
+    uint16_t nrst;				/* Restart inverval */
+    uint16_t width, height;		/* Size of the input image (pixel) */
+    uint8_t* huffbits[2][2];	/* Huffman bit distribution tables [id][dcac] */
+    uint16_t* huffcode[2][2];	/* Huffman code word tables [id][dcac] */
+    uint8_t* huffdata[2][2];	/* Huffman decoded data tables [id][dcac] */
+    int32_t* qttbl[4];			/* Dequantizer tables [id] */
+    void* workbuf;				/* Working buffer for IDCT and RGB output */
+    uint8_t* mcubuf;			/* Working buffer for the MCU */
+    void* pool;					/* Pointer to available memory pool */
+    uint16_t sz_pool;			/* Size of momory pool (bytes available) */
+    uint16_t (*infunc)(JDEC*, uint8_t*, uint16_t);/* Pointer to jpeg stream input function */
+    void* device;				/* Pointer to I/O device identifiler for the session */
 };
 
 
