9c9c464ab5e75623a7a2fa650fb960054c947796
Mika Tuupola <tuupola@appelsiini.net>
2023-02-04 23:24:56 +0700

    Remove old bitmap_init()

--- include/hagl/bitmap.h
+++ include/hagl/bitmap.h
@@ -68,7 +68,6 @@ typedef struct {
 } hagl_bitmap_t;
 
 uint32_t bitmap_size(hagl_bitmap_t *bitmap);
-void bitmap_init(hagl_bitmap_t *bitmap, uint8_t *buffer);
 void hagl_bitmap_init(hagl_bitmap_t *bitmap, int16_t width, uint16_t height, uint8_t depth, void *buffer);
 
 #ifdef __cplusplus
