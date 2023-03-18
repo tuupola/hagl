tuupola/hagl

commit 81ee12a552c21d031003fcad6c9f36c1d57949f8

Fix off by one error in hline and vline (#110)

src/hagl_bitmap.c
@@ -64,7 +64,7 @@ hline(void *_bitmap, int16_t x0, int16_t y0, uint16_t width, hagl_color_t color)
     hagl_bitmap_t *bitmap = _bitmap;
 
     hagl_color_t *ptr = (hagl_color_t *) (bitmap->buffer + bitmap->pitch * y0 + (bitmap->depth / 8) * x0);
-    for (uint16_t x = 0; x < width; x++) {
+    for (uint16_t x = 0; x <= width; x++) {
         *ptr++ = color;
     }
 }
@@ -75,7 +75,7 @@ vline(void *_bitmap, int16_t x0, int16_t y0, uint16_t height, hagl_color_t color
     hagl_bitmap_t *bitmap = _bitmap;
 
     hagl_color_t *ptr = (hagl_color_t *) (bitmap->buffer + bitmap->pitch * y0 + (bitmap->depth / 8) * x0);
-    for (uint16_t y = 0; y < height; y++) {
+    for (uint16_t y = 0; y <= height; y++) {
         *ptr = color;
         ptr += bitmap->pitch / (bitmap->depth / 8);
     }

src/hagl_hline.c
@@ -56,17 +56,17 @@ hagl_draw_hline_xyw(void const *_surface, int16_t x0, int16_t y0, uint16_t w, ha
         }
 
         /* Everything outside clip window, nothing to do. */
-        if (width < 0)  {
+        if (width <= 0)  {
             return;
         }
 
         /* Cut anything going over right edge of clip window. */
         if (((x0 + width) > surface->clip.x1)) {
-            width = width - (x0 + width - surface->clip.x1);
+            width = width - (x0 + width - 1 - surface->clip.x1);
         }
 
         surface->hline(&surface, x0, y0, width, color);
     } else {
-        hagl_draw_line(surface, x0, y0, x0 + w, y0, color);
+        hagl_draw_line(surface, x0, y0, x0 + w - 1, y0, color);
     }
 }

src/hagl_vline.c
@@ -56,17 +56,17 @@ hagl_draw_vline_xyh(void const *_surface, int16_t x0, int16_t y0, uint16_t h, ha
         }
 
         /* Everything outside clip window, nothing to do. */
-        if (height < 0)  {
+        if (height <= 0)  {
             return;
         }
 
         /* Cut anything going over right edge. */
         if (((y0 + height) > surface->clip.y1))  {
-            height = height - (y0 + height - surface->clip.y1);
+            height = height - (y0 + height - 1 - surface->clip.y1);
         }
 
         surface->vline(&surface, x0, y0, height, color);
     } else {
-        hagl_draw_line(surface, x0, y0, x0, y0 + h, color);
+        hagl_draw_line(surface, x0, y0, x0, y0 + h - 1, color);
     }
 }

commit 125804efb7d7ac0799938b6d422645ddf469a0a7

Fix another off by one error introduced by #110

src/hagl_bitmap.c
@@ -64,7 +64,7 @@ hline(void *_bitmap, int16_t x0, int16_t y0, uint16_t width, hagl_color_t color)
     hagl_bitmap_t *bitmap = _bitmap;
 
     hagl_color_t *ptr = (hagl_color_t *) (bitmap->buffer + bitmap->pitch * y0 + (bitmap->depth / 8) * x0);
-    for (uint16_t x = 0; x <= width; x++) {
+    for (uint16_t x = 0; x < width; x++) {
         *ptr++ = color;
     }
 }
@@ -75,7 +75,7 @@ vline(void *_bitmap, int16_t x0, int16_t y0, uint16_t height, hagl_color_t color
     hagl_bitmap_t *bitmap = _bitmap;
 
     hagl_color_t *ptr = (hagl_color_t *) (bitmap->buffer + bitmap->pitch * y0 + (bitmap->depth / 8) * x0);
-    for (uint16_t y = 0; y <= height; y++) {
+    for (uint16_t y = 0; y < height; y++) {
         *ptr = color;
         ptr += bitmap->pitch / (bitmap->depth / 8);
     }