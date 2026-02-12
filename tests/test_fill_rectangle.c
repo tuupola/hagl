926802d75836404d1adb9ccdea7ee65f8d79a467
Mika Tuupola <tuupola@appelsiini.net>
2026-02-12 14:44:26 +0200

    Add test for matching filled rectange xyxy and xywh output

--- tests/test_fill_rectangle.c
+++ tests/test_fill_rectangle.c
@@ -116,10 +116,29 @@ test_fill_rectangle_xyxy_regression(void) {
     PASS();
 }
 
+TEST
+test_fill_rectangle_xyxy_match_xywh(void) {
+    /* Draw with _xyxy. */
+    hagl_fill_rectangle_xyxy(&backend, 10, 10, 20, 20, 0xFFFF);
+
+    size_t size = backend.width * backend.height * (backend.depth / 8);
+    uint32_t crc_xyxy = crc32(backend.buffer, size);
+
+    /* Clear and draw the same area with _xywh. */
+    memset(backend.buffer, 0, size);
+    hagl_fill_rectangle_xywh(&backend, 10, 10, 11, 11, 0xFFFF);
+
+    uint32_t crc_xywh = crc32(backend.buffer, size);
+
+    ASSERT_EQ(crc_xyxy, crc_xywh);
+    PASS();
+}
+
 SUITE(fill_rectangle_suite) {
     SET_SETUP(setup_callback, NULL);
     RUN_TEST(test_fill_rectangle_xyxy);
     RUN_TEST(test_fill_rectangle_xyxy_regression);
+    RUN_TEST(test_fill_rectangle_xyxy_match_xywh);
 }
 
 GREATEST_MAIN_DEFS();
