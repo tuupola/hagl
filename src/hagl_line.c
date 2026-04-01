b364f4a90ca2a824df105f6ec087dc33554beebe
Mika Tuupola <tuupola@appelsiini.net>
2026-04-01 19:16:13 +0300

    Remove unneseccary casts

--- src/hagl_line.c
+++ src/hagl_line.c
@@ -65,7 +65,7 @@ void hagl_draw_line(
     err = (dx > dy ? dx : -dy) / 2;
 
     while (1) {
-        surface->put_pixel((void *)_surface, x0, y0, color);
+        surface->put_pixel(_surface, x0, y0, color);
 
         if ((x0 == x1) && (y0 == y1)) {
             break;
@@ -83,7 +83,7 @@ void hagl_draw_line(
             y0 += sy;
         }
 
-        surface->put_pixel((void *)_surface, x0, y0, color);
+        surface->put_pixel(_surface, x0, y0, color);
 
         if ((x0 == x1) && (y0 == y1)) {
             break;
