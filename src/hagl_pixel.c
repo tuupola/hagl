165ce88d590dd4cefc198522d9e63dcc5bf85bd9
Mika Tuupola <tuupola@appelsiini.net>
2026-09-16 18:00:30 +0300

    Make put pixel static inline

--- src/hagl_pixel.c
+++ src/hagl_pixel.c
@@ -37,23 +37,6 @@ SPDX-License-Identifier: MIT
 #include "hagl/color.h"
 #include "hagl/surface.h"
 
-void hagl_put_pixel(void const *_surface, int16_t x0, int16_t y0, hagl_color_t color) {
-    const hagl_surface_t *surface = _surface;
-
-    /* x0 or y0 is before the edge, nothing to do. */
-    if ((x0 < surface->clip.x0) || (y0 < surface->clip.y0)) {
-        return;
-    }
-
-    /* x0 or y0 is after the edge, nothing to do. */
-    if ((x0 > surface->clip.x1) || (y0 > surface->clip.y1)) {
-        return;
-    }
-
-    /* If still in bounds set the pixel. */
-    surface->put_pixel(_surface, x0, y0, color);
-}
-
 hagl_color_t hagl_get_pixel(void const *_surface, int16_t x0, int16_t y0) {
     const hagl_surface_t *surface = _surface;
     /* x0 or y0 is before the edge, nothing to do. */
